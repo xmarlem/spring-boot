7519182cc80afee12922f98613ecc71143331c2e
xmarlem <inglembomarco@gmail.com>
2018-05-16 22:41:02 +0200

    added comment

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
@@ -1,4 +1,4 @@
-/*
+
  * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -80,6 +80,7 @@ public class SampleAtmosphereApplication {
 	}
 
 	public static void main(String[] args) {
+		//my comment
 		SpringApplication.run(SampleAtmosphereApplication.class, args);
 	}
 
